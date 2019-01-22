73ba106becb7de1b04e602cf5d7725110063565c
Xiaolei Yang <xuanyuan95@126.com>
2019-01-23 01:42:40 +0800

    添加web文件夹

---
+++ web/controller/index.php
@@ -0,0 +1,12 @@
+<?php namespace web\controller;
+class index {
+    public function show() {
+        echo '半神半圣亦半仙';
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: yangxiaolei
+ * Date: 2019/1/23
+ * Time: 01:39
+ */
