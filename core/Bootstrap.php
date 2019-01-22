016dd19b51229ef6bd99ce93e64806fc434635d8
Xiaolei Yang <xuanyuan95@126.com>
2019-01-23 03:06:04 +0800

    全部上传

---
+++ core/Bootstrap.php
@@ -0,0 +1,12 @@
+<?php namespace core;
+class Bootstrap {
+    public static function run () {
+
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: yangxiaolei
+ * Date: 2019/1/23
+ * Time: 01:49
+ */
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include "vendor/autoload.php";
 Bootstrap::run();
 /**
  * Created by PhpStorm.
